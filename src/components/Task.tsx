e6bb3af8d9fa235bdcdf08c0294d969ebb5e23f2
Albarral19 <jalbarral96@gmail.com>
2024-06-19 19:33:55 -0300

    revert functions change

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,36 +2,23 @@ import { Trash, Check } from "@phosphor-icons/react";
 import { TaskProps } from "../Types";
 
 export function Task({ task, onChangeTaskStatus, onDeleteTask }: TaskProps) {
-  function handleChange() {
-    onChangeTaskStatus(task);
-  }
-
-  function handleDelete() {
-    onDeleteTask(task);
-  }
-
   return (
     <div className="flex items-start gap-3 rounded-lg bg-zinc-800 p-4">
-      <label
-        className="my-1 flex cursor-pointer items-center"
-        htmlFor={`checkboxTask-${task.id}`}
-      >
+      <label className="my-1 cursor-pointer">
         <input
           type="checkbox"
-          name="checkboxTask"
-          id={`checkboxTask-${task.id}`}
-          onChange={handleChange}
+          onChange={onChangeTaskStatus(task)}
           className="hidden"
           checked={task.isCompleted}
         />
         <div
-          className={`flex h-6 w-6 items-center justify-center rounded-full transition-all ${
+          className={`flex size-4 items-center justify-center rounded-full transition-all ${
             task.isCompleted
-              ? "bg-indigo-600 hover:bg-indigo-500"
+              ? "border-none bg-indigo-600 hover:bg-indigo-500"
               : "border border-sky-500 hover:border-sky-400"
           }`}
         >
-          {task.isCompleted && <Check size={16} weight="bold" />}
+          {task.isCompleted && <Check size={10} weight="bold" />}
         </div>
       </label>
 
@@ -44,7 +31,7 @@ export function Task({ task, onChangeTaskStatus, onDeleteTask }: TaskProps) {
       </p>
       <button
         className="cursor-pointer rounded text-zinc-500 hover:bg-zinc-700 hover:text-red-400"
-        onClick={handleDelete}
+        onClick={onDeleteTask(task)}
       >
         <Trash className="my-1" width={24} weight="bold" />
       </button>
