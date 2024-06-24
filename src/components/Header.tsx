e3b354231410b1f7681725b415862b03f213c602
José Augusto Albarral <49702703+Albarral19@users.noreply.github.com>
2024-06-24 12:28:33 +0000

    fix w screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "../assets/Logo.svg";
 
 export function Header() {
   return (
-    <header className="flex h-52 w-screen items-center justify-center bg-zinc-950">
+    <header className="flex h-52 items-center justify-center bg-zinc-950">
       <img className="h-20" src={Logo} alt="Logo do to-do list" />
     </header>
   );
